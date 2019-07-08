e64b3990d6c11c7b2219cec4f81c86d11e36952c
Tung <ntung7965@gmail.com>
2019-07-08 22:50:53 +0700

    dfd

--- Project/js/home.js
+++ Project/js/home.js
@@ -1,27 +1,36 @@
 let a = '';
 for (i = 1; i <= 8; i++) {
 
-    a += `<div class="card col-md-3 col-sm-6">
-    <img src="../img/sản phẩm ${i}.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text"><span style="text-decoration:line-through;font-size:0.7rem;color:grey">600.000đ</span>~400.000đ</p>
+    a += `<div class="col-lg-3 col-md-4 col-sm-6">
+    <div class="product">
+        <div class="imgbox">
+            <img src="../img/sản phẩm ${i}.jpg" alt="">
+
+            <div class="details">
+
+                <a href=""><i class="fas fa-cart-arrow-down"></i></a>
+                <a href=""><i class="fas fa-search-plus"></i></a>
+                <a href="item.html">Mua Ngay</a>
+            </div>
+        </div>
+
+        <div class="info">
+            <div style="padding-bottom: 15px">
+                <h6>Brand Name<br><span>Giày XXX</span></h6>
+                <div class="price">$55.55</div>
+            </div>
+        </div>
     </div>
-    
-    <div class="card-body">
-    <a href="item.html"><button type="button" class="btn btn-danger">Danger</button></a>
-    <a href="item.html"><button type="button" class="btn btn-danger">Danger</button></a>
-    </div>
-  </div>`
+</div>`
 }
 
 document.getElementById('newitem').innerHTML = a;
 document.getElementById('hotitem').innerHTML = a;
 
 
-let b='';
-for(i=0;i<3;i++){
-  b+=`<div id="comment" class="col" style="padding: 1.2rem">
+let b = '';
+for (i = 0; i < 3; i++) {
+    b += `<div id="comment" class="col" style="padding: 1.2rem">
   <div id="comment_1" style="width:5rem">
       <img src="../img/chandung.png" alt="">
   </div>
@@ -36,9 +45,9 @@ for(i=0;i<3;i++){
 document.getElementById('main_4_1').innerHTML = b;
 
 
-let c='';
-for(i=0;i<3;i++){
-  c+=`<div id="news" class="col" style="padding: 1.2rem">
+let c = '';
+for (i = 0; i < 3; i++) {
+    c += `<div id="news" class="col" style="padding: 1.2rem">
   
   <div id="news_1">
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. in the 1960s with the
