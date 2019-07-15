5495ec14054235336e093dfc981ea0fe1d2d5e87
Tung <ntung7965@gmail.com>
2019-07-15 22:31:10 +0700

    xong header

---
+++ Project/js/header_footer.js
@@ -0,0 +1,59 @@
+// Search 
+
+function search_responsive() {
+    let x = document.getElementById('input_search');
+
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+
+
+}
+
+// 
+
+
+
+
+// Modal
+
+$('.message a').click(function () {
+    $('.form_1').animate({
+        height: "toggle",
+        opacity: "toggle"
+    }, "slow");
+});
+
+
+function login() {
+
+    let a = document.getElementById('login-page');
+
+    if (a.style.display === 'block') {
+        a.style.display = 'none'
+    } else {
+        a.style.display = 'block';
+    }
+    document.getElementById('fullscreen').style.visibility = 'visible';
+    document.getElementById('fullscreen').style.opacity = 1;
+    document.getElementById('fullscreen').style.zIndex = 1000;
+
+}
+
+
+function off() {
+    let a = document.getElementById('login-page');
+
+    if (a.style.display === 'block') {
+        a.style.display = 'none'
+    }
+
+    document.getElementById('fullscreen').style.visibility = 'hidden';
+    document.getElementById('fullscreen').style.opacity = 0;
+    document.getElementById('fullscreen').style.zIndex = -1000;
+
+}
+// 
