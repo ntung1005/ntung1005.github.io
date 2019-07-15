51e35335f07b7a8525c27727fce8b2baa847d23f
Tung <ntung7965@gmail.com>
2019-07-15 19:01:48 +0700

    dsfd

--- header-Project/header.js
+++ header-Project/header.js
@@ -1,5 +1,27 @@
+// Search 
+
+function search_responsive() {
+    let x = document.getElementById('input_search');
+
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+    
+
+}
+
+// 
+
+
+
+
+// Modal
+
 $('.message a').click(function () {
-    $('form').animate({
+    $('.form_1').animate({
         height: "toggle",
         opacity: "toggle"
     }, "slow");
@@ -17,6 +39,8 @@ function login() {
     }
     document.getElementById('fullscreen').style.visibility = 'visible';
     document.getElementById('fullscreen').style.opacity = 1;
+    document.getElementById('fullscreen').style.zIndex=1000;
+    
 }
 
 
@@ -29,5 +53,7 @@ function off() {
 
     document.getElementById('fullscreen').style.visibility = 'hidden';
     document.getElementById('fullscreen').style.opacity = 0;
+    document.getElementById('fullscreen').style.zIndex=-1000;
 
 }
+// 
