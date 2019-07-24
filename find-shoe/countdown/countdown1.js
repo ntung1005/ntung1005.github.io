834bd663f2841b31b3a40b24c38603ea7ffccc9b
Tung <ntung7965@gmail.com>
2019-07-24 17:11:53 +0700

    adss

--- find-shoe/countdown/countdown1.js
+++ find-shoe/countdown/countdown1.js
@@ -1,6 +1,6 @@
-$('#banner #clock').countdown('2019/8/5', function (event) {
+$('#banner #clock').countdown('2019/7/28', function (event) {
     let $this = $(this).html(event.strftime('' +
-        '<div><span>%w</span><br><span>Tuần</span></div>' +
+        
         '<div><span>%d</span><br><span>Ngày</span></div>' +
         '<div><span>%H</span><br><span>Giờ</span></div>' +
         '<div><span>%M</span><br><span>Phút </span></div>' +
--- find-shoe/js/header_footer.js
+++ find-shoe/js/header_footer.js
@@ -99,6 +99,11 @@ function off() {
     document.getElementById('fullscreen').style.zIndex = -1000;
 
 }
+
+
+
+
+
 // 
 
 
@@ -107,6 +112,33 @@ function off() {
 
 
 
+
+
+
+
+
+// Plugin ChatBox
+
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v3.3'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
+// 
 
 
 
