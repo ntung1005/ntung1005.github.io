425a36c721a67226859f89db90c1e263b96304a9
Tung <ntung7965@gmail.com>
2019-07-24 18:21:34 +0700

    sds

--- find-shoe/js/header_footer.js
+++ find-shoe/js/header_footer.js
@@ -1,3 +1,35 @@
+// Plugin ChatBox
+
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v3.3'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
 // Fix-Header
 
 let nav=document.getElementById('header_responsive');
@@ -117,28 +149,7 @@ function off() {
 
 
 
-// Plugin ChatBox
 
-window.fbAsyncInit = function () {
-    FB.init({
-        xfbml: true,
-        version: 'v3.3'
-    });
-};
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
-// 
 
 
 
