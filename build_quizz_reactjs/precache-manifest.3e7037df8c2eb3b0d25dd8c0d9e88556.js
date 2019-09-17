557dd63612d1c219bbec26d20e47b7b97f48a8b2
ntung1005 <ntung7965@gmail.com>
2019-09-17 15:34:02 +0700

    aaa

---
+++ build_quizz_reactjs/precache-manifest.3e7037df8c2eb3b0d25dd8c0d9e88556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee6b6613da7b53908932d2c1bb0b391",
+    "url": "/build_quizz_reactjs/index.html"
+  },
+  {
+    "revision": "bd72e60cd6d79f5f8f93",
+    "url": "/build_quizz_reactjs/static/css/main.5e692ea6.chunk.css"
+  },
+  {
+    "revision": "32061f2f4fe1438d5314",
+    "url": "/build_quizz_reactjs/static/js/2.67be95f2.chunk.js"
+  },
+  {
+    "revision": "bd72e60cd6d79f5f8f93",
+    "url": "/build_quizz_reactjs/static/js/main.6dfc8677.chunk.js"
+  },
+  {
+    "revision": "b115f5057b21cde044d5",
+    "url": "/build_quizz_reactjs/static/js/runtime~main.502dfdf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build_quizz_reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
