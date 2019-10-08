b7ca5527d8ea9170d193ac628b91b35a71adf554
ntung1005 <ntung7965@gmail.com>
2019-10-08 09:22:06 +0700

    end-home-ofelia

--- ofelia/js/home.js
+++ ofelia/js/home.js
@@ -1,28 +1,6 @@
-// Fix header
-window.onscroll = function() {
-  myFunction();
-};
 
-let header = document.getElementById("header");
-let sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
-// Header hamburger
-
-$(".hamburger-bars").click(function() {
-  $(".mobile-nav").addClass("add");
-});
-
-$(".close-nav").click(function() {
-  $(".mobile-nav").removeClass("add");
-});
 
 // Products
 
