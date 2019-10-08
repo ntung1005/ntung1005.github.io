648d5c3f9ea58ffcb8166a1501385e71b02e895c
ntung1005 <48557699+ntung1005@users.noreply.github.com>
2019-10-09 00:12:13 +0700

    Update single-product.js

--- ofelia/js/single-product.js
+++ ofelia/js/single-product.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop: true,
         margin: 10,
-        nav: true,
         responsive: {
             0: {
                 items: 3
