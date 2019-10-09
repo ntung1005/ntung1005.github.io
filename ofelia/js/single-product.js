ca6c659a480446703f105ca719a5d0cacac624a6
ntung1005 <ntung7965@gmail.com>
2019-10-09 11:32:32 +0700

    asdasd

---
+++ ofelia/js/single-product.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 3
+            },
+            
+        }
+    });
+});
+
+
+
+
+$('.btn-evaluate').click(function(){
+    $(".evaluate-content").slideToggle();
+})
