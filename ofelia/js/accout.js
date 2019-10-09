7da741ff3cb6107a3d3b51a13ed78e851dd74625
ntung1005 <ntung7965@gmail.com>
2019-10-09 11:09:35 +0700

    sad

---
+++ ofelia/js/accout.js
@@ -0,0 +1,10 @@
+$(".btn-forget-password").click(function(){
+    $(".login-content").hide();
+    $(".forget-password").show();
+})
+
+
+$(".exit-forget-pasword").click(function(){
+    $(".login-content").show();
+    $(".forget-password").hide();
+})
