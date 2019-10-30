ef2b8bdc9d7a62bc02de901cc1606da7ae707ff5
ntung1005 <ntung7965@gmail.com>
2019-10-30 20:01:04 +0700

    adsa

---
+++ MaxStore/js/header.js
@@ -0,0 +1,7 @@
+$('.hamburger_container').click(function(){
+    $('.hamburger_menu').addClass('active')
+})
+
+$(".hamburger_close").click(function(){
+    $('.hamburger_menu').removeClass('active')
+})
