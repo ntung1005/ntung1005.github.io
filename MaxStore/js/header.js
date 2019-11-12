0389ddb5bf192881984b6d74acddd8828f17169d
ntung1005 <ntung7965@gmail.com>
2019-11-12 16:05:51 +0700

    aa

--- MaxStore/js/header.js
+++
@@ -1,7 +0,0 @@
-$('.hamburger_container').click(function(){
-    $('.hamburger_menu').addClass('active')
-})
-
-$(".hamburger_close").click(function(){
-    $('.hamburger_menu').removeClass('active')
-})
